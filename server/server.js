e269042bf569010b5f82e5312a2591d3ae3e998f
salimkhandev <salimt450s@gmail.com>
2025-04-24 22:50:57 +0500

    improved

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ io.on("connection", (socket) => {
 });
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT} 🚀`);
 });

8f684e92c30d7ce3373d4f96d9afe9fbb744a404
salimkhandev <salimt450s@gmail.com>
2025-04-25 00:29:56 +0500

    improved

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to ChatterSocket!" });
+    res.json({ message: "Welcome to ChatterSocket! we are live" });
 });
 
 // Socket.IO logic

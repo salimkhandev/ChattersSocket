ab595d28ba8868650ad26383ca81d593c0e11cc8
salimkhandev <salimt450s@gmail.com>
2025-04-27 00:53:18 +0500

    improved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+// const socket = io("http://localhost:3000");
+const socket = io("https://chatterssocket-production.up.railway.app/");
 
 export default function ChatApp() {
   const [username, setUsername] = useState(localStorage.getItem("chat_user") || "");

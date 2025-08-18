04333f0e51b6c59d4630ae7ef07e09f938fb4f6e
salimkhandev <salimt450s@gmail.com>
2025-08-18 18:34:48 +0500

    improved

--- server/index.js
+++ server/index.js
@@ -41,10 +41,18 @@ app.use('/', uploadMedia);
 try {
     admin.initializeApp({
         credential: admin.credential.cert({
-            projectId: process.env.PROJECT_ID,
-            clientEmail: process.env.CLIENT_EMAIL,
-            privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-        })
+            type: process.env.TYPE,
+            project_id: process.env.PROJECT_ID,
+            private_key_id: process.env.PRIVATE_KEY_ID,
+            private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+            client_email: process.env.CLIENT_EMAIL,
+            client_id: process.env.CLIENT_ID,
+            auth_uri: process.env.AUTH_URI,
+            token_uri: process.env.TOKEN_URI,
+            auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+            client_x509_cert_url: process.env.CLIENT_CERT_URL,
+            universe_domain: process.env.UNIVERSE_DOMAIN,
+        }),
     });
     console.log('Firebase Admin initialized successfully');
 } catch (error) {
